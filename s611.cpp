d31c461a58f9fbcf9896691841b3bd8ea02f6666
Adarshika Pandey <153723733+adarshika2005@users.noreply.github.com>
2024-12-06 23:27:57 +0530

    Add files via upload

---
+++ s611.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int l=2;
+    int b=3;
+    int a=l*b;
+    cout<<"area of rectangle is "<<a<<endl;
+
+}
---
+++ s612.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int area,length,breath;
+    cout<<"enter the length ";
+    cin>>length;
+    cout<<"enter the breath ";
+    cin>>breath;
+    area=length*breath;
+    cout<<"area of rectangle "<<area<<endl;
+
+}
